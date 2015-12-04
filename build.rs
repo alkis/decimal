a07a39b08588be3c8c14f386ca707afb8407f9bd
Alkis Evlogimenos <alkis@evlogimenos.com>
2015-12-04 17:37:44 +0100

    Rustfmt.

--- build.rs
+++ build.rs
@@ -2,15 +2,15 @@ extern crate gcc;
 
 fn main() {
     let mut cfg = gcc::Config::new()
-    .include("decNumber")
-    .file("decNumber/decContext.c")
-    .file("decNumber/decDouble.c")
-    .file("decNumber/decNumber.c")
-    .file("decNumber/decPacked.c")
-    .file("decNumber/decQuad.c")
-    .file("decNumber/decSingle.c")
-    .file("decNumber/decimal128.c")
-    .file("decNumber/decimal32.c")
-    .file("decNumber/decimal64.c")
-    .compile("libdecNumber.a");
+                      .include("decNumber")
+                      .file("decNumber/decContext.c")
+                      .file("decNumber/decDouble.c")
+                      .file("decNumber/decNumber.c")
+                      .file("decNumber/decPacked.c")
+                      .file("decNumber/decQuad.c")
+                      .file("decNumber/decSingle.c")
+                      .file("decNumber/decimal128.c")
+                      .file("decNumber/decimal32.c")
+                      .file("decNumber/decimal64.c")
+                      .compile("libdecNumber.a");
 }
