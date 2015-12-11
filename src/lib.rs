84ebbd69555c4d7210795aeca79d1d98d4c64f0f
Alkis Evlogimenos <alkis@evlogimenos.com>
2015-12-11 18:44:58 +0100

    Fully qualify call to from_str in macro.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ extern crate quick_error;
 macro_rules! d128 {
     ($lit:expr) => {{
         use std::str::FromStr;
-        d128::from_str(stringify!($lit)).expect("Invalid decimal float literal")
+        $crate::d128::from_str(stringify!($lit)).expect("Invalid decimal float literal")
     }}
 }
 
