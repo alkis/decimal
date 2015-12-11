211794776782c03de5b613b6f7e93202a50570ee
Alkis Evlogimenos <alkis@evlogimenos.com>
2015-12-11 20:11:26 +0100

    Document lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,18 @@ extern crate bitflags;
 extern crate libc;
 
 #[macro_export]
+/// A macro to construct d128 literals.
+///
+/// # Examples:
+/// ```
+/// # #[macro_use]
+/// # extern crate decimal;
+/// # fn main() {
+/// assert!(d128!(NaN).is_nan());
+/// assert!(d128!(0).is_zero());
+/// assert!(d128!(-0.1).is_negative());
+/// # }
+/// ```
 macro_rules! d128 {
     ($lit:expr) => {{
         use std::str::FromStr;
@@ -21,6 +33,8 @@ pub enum Rounding {
     Ceiling = 0,
     Up,
     HalfUp,
+    /// Round to nearest; if equidistant, round so that the final digit is even.
+    /// This is the only rounding mode supported.
     HalfEven,
     HalfDown,
     Down,
@@ -45,20 +59,49 @@ pub enum Class {
 }
 
 bitflags! {
+    /// The status of the floating point context. Instead of faulting after every operation errors
+    /// are added to this status. User code can check and/or clear the status fully or partially at
+    /// specific points to check the validity of the computation. Each thread gets its own status.
+    /// The status is initially empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[macro_use]
+    /// # extern crate decimal;
+    /// # use decimal::d128;
+    /// # use decimal::Status;
+    /// # fn main() {
+    /// assert_eq!(d128::get_status(), Status::empty());
+    /// let _ = d128!(1) / &d128!(0);
+    /// assert!(d128::get_status().contains(decimal::DIVISION_BY_ZERO));
+    /// let _ = d128!(0) / &d128!(0);
+    /// assert!(d128::get_status().contains(decimal::DIVISION_UNDEFINED));
+    /// // The previous status flag was not cleared!
+    /// assert!(d128::get_status().contains(decimal::DIVISION_BY_ZERO));
+    /// # }
     flags Status: libc::uint32_t {
+        /// Conversion syntax error.
         const CONVERSION_SYNTAX    = 0x00000001,
+        /// Division by zero.
         const DIVISION_BY_ZERO     = 0x00000002,
+        /// Division impossible.
         const DIVISION_IMPOSSIBLE  = 0x00000004,
+        /// Division undefined.
         const DIVISION_UNDEFINED   = 0x00000008,
-        const INSUFFICIENT_STORAGE = 0x00000010,
+        // const INSUFFICIENT_STORAGE = 0x00000010,
+        /// The result is inexact.
         const INEXACT              = 0x00000020,
-        const INVALID_CONTEXT      = 0x00000040,
+        // const INVALID_CONTEXT      = 0x00000040,
+        /// An invalid operation was requested.
         const INVALID_OPERATION    = 0x00000080,
-        const LOST_DIGITS          = 0x00000100,
+        // const LOST_DIGITS          = 0x00000100,
+        /// Overflow.
         const OVERFLOW             = 0x00000200,
-        const CLAMPED              = 0x00000400,
-        const ROUNDED              = 0x00000800,
-        const SUBNORMAL            = 0x00001000,
+        // const CLAMPED              = 0x00000400,
+        // const ROUNDED              = 0x00000800,
+        // const SUBNORMAL            = 0x00001000,
+        /// Underflow.
         const UNDERFLOW            = 0x00002000,
     }
 }
