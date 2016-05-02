94d95698f5d79a8678bd58faf57ecaf9ea484ae3
Mark Sinclair II <125axel125@gmail.com>
2016-05-02 00:11:38 -0500

    Added serde serialization and deserialization as a feature

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ extern crate libc;
 extern crate ord_subset;
 #[cfg(feature = "rustc-serialize")]
 extern crate rustc_serialize;
+#[cfg(feature = "serde")]
+extern crate serde;
 
 #[macro_export]
 /// A macro to construct d128 literals.
