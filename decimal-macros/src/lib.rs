fbbc7c39163a9c2a467ea02e8aec5a6c4bde4c8c
trolleyman <cgtrolley@gmail.com>
2016-05-10 11:06:33 +0100

    Added comments

--- decimal-macros/src/lib.rs
+++ decimal-macros/src/lib.rs
@@ -16,11 +16,14 @@ use syntax::ast::{ExprKind, TokenTree, LitKind, StrStyle};
 use decimal::d128;
 
 fn d128_lit<'cx>(cx: &'cx mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<MacResult + 'cx> {
+    // Turn input into a string literal
+    // e.g. d128!(0.1) -> "0.1", d128!(NaN) -> "NaN"
     let mac_res = source_util::expand_stringify(cx, sp, tts);
     
     let ex = match mac_res.make_expr() {
         Some(ex) => ex,
         None => {
+            // I don't know when this occurs
             cx.span_err(sp, "one argument needed");
             return DummyResult::any(sp);
         }
@@ -29,6 +32,8 @@ fn d128_lit<'cx>(cx: &'cx mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<MacRe
     match &ex.node {
         &ExprKind::Lit(ref lit) => match &lit.node {
             &LitKind::Str(ref s, StrStyle::Cooked) => {
+                //cx.span_warn(lit.span, &format!("Parsing \"{}\"...", s));
+                // Check for empty argument
                 if (*s).len() == 0 {
                     cx.span_err(sp, "one argument needed");
                     return DummyResult::any(sp);
@@ -39,23 +44,27 @@ fn d128_lit<'cx>(cx: &'cx mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<MacRe
                         cx.span_err(lit.span, s);
                         return DummyResult::any(sp);
                     }
-                };
+                }; 
                 let num = unsafe { ::std::mem::transmute::<d128, [u8; 16]>(num) };
                 
+                // cx.span_warn(lit.span, &format!("{} -> {:?}", s, num));
+                
+                // Create array literal
                 let mut vec = Vec::new();
                 for i in 0..16 {
                     vec.push(cx.expr_u8(lit.span, num[i]));
                 }
                 let vec = cx.expr_vec(lit.span, vec);
                 let ids = vec![cx.ident_of("decimal"), cx.ident_of("d128"), cx.ident_of("from_bytes")];
-                let ex = cx.expr_call_global(lit.span, ids, vec![vec]);
-
+                let ex = cx.expr_call_global(lit.span, ids, vec![vec]); // Call ::decimal::d128::from_bytes with array literal
+                
                 return MacEager::expr(ex);
             },
             _ => {}
         },
         _ => {}
     }
+    // This should never happen.
     cx.span_err(sp, "not a valid d128 number");
     DummyResult::any(sp)
 }
