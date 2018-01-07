890e966d95bc9179246e266c1604b060557cde0a
Daniel Rodgers-Pryor <djrodgerspryor@gmail.com>
2018-01-08 05:29:35 +1100

    Add ::infinity and ::neg_infinity functions (#44)

--- src/dec128.rs
+++ src/dec128.rs
@@ -549,6 +549,16 @@ impl d128 {
         }
     }
 
+    /// Returns the d128 representing +Infinity.
+    pub fn infinity() -> d128 {
+        d128!(Infinity)
+    }
+
+    /// Returns the d128 representing -Infinity.
+    pub fn neg_infinity() -> d128 {
+        d128!(-Infinity)
+    }
+
     // Computational.
 
     /// Returns the absolute value of `self`.
@@ -992,6 +1002,17 @@ mod tests {
     fn default() {
         assert_eq!(d128::zero(), d128::default());
         assert_eq!(d128::zero(), Default::default());
+    }
+
+    #[test]
+    fn special() {
+        assert!(d128::infinity().is_infinite());
+        assert!(!d128::infinity().is_negative());
+
+        assert!(d128::neg_infinity().is_infinite());
+        assert!(d128::neg_infinity().is_negative());
+
+        assert_eq!(d128::infinity() + d128!(1), d128::infinity());
     }
 
     #[cfg(feature = "ord_subset")]
